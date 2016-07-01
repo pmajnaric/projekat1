7885643a1ffa12a371d2abe1c7947fc40e521889
pmajnaric <paulina.majnaric1@gmail.com>
2016-07-02 00:02:33 +0200

    template f-cija

--- include/app.php
+++ include/app.php
@@ -3,26 +3,46 @@
 error_reporting(E_ALL);
 
 require_once "/const.php";
-require_once "template/header.tpl.php";
+
 
 class App
 {
 	
 	private static $defTitle='Novi sajt';
-	private static $meta_page=[];
+	private static $page_meta=[];
+	private static $vars=[];
+	private static $prefix='template/';
+	private static $sufix='.tpl.php';
 	
 	
 	public static function setTitle($title,$addTitle=true){
 		
 		if(empty($title)){return;}
-		if($addTitle){$title=self::$defTitle."{$title}";}
+		if($addTitle){$title=self::$defTitle."-{$title}";}
 		 self::$page_meta['title']=$title;	
 		}
 	
 	public static function getTitle(){
 		
 		return !empty(self::$page_meta['title']) ? self::$page_meta['title'] : self::$defTitle;
-	} 
+	}
+
+	
+	public static function template ($template)
+	{
+		if(empty($template)){return;}
+		if(!is_array($template)){$template=array($template);}
+		
+		foreach(self::$vars as $key=>$val)
+		{
+			$$key=$val;	
+		}
+		foreach($template as $val)
+		{
+			require self::$prefix.$val.self::$sufix;
+		}
+	}
+		
 	
 };
 
@@ -30,36 +50,4 @@ class App
 
 
 
-?><?php
 
-error_reporting(E_ALL);
-
-require_once "/const.php";
-require_once "template/header.tpl.php";
-
-class App
-{
-	
-	private static $defTitle='Novi sajt';
-	private static $meta_page=[];
-	
-	
-	public static function setTitle($title,$addTitle=true){
-		
-		if(empty($title)){return;}
-		if($addTitle){$title=self::$defTitle."{$title}";}
-		 self::$page_meta['title']=$title;	
-		}
-	
-	public static function getTitle(){
-		
-		return !empty(self::$page_meta['title']) ? self::$page_meta['title'] : self::$defTitle;
-	} 
-	
-};
-
-
-
-
-
-?>
