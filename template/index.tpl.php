ddc7a4d1a90e2b1d371626d81672ef7b3fe03e0c
pmajnaric <paulina.majnaric1@gmail.com>
2016-07-11 23:55:52 +0200

    .

--- template/index.tpl.php
+++ template/index.tpl.php
@@ -1,13 +1,33 @@
 
 
 
-
 <div class="wrapper">
 
-nesto
+	<div class="sent">
 
-
-
+	<p id="Leonardo"><a>"Jednostavnost je krajnji izraz prefinjenosti"<a>
+	<br>
+	<a>Leonardo Da Vinci</a></p>
+	
+	<p id="Stiv"><a>Ubeđen sam da je čista upornost polovina onoga što razdvaja uspešne preduzetnike od neuspešnih</a>
+	<br>
+	<a>Stiv Dzobs</a></p>
+	<script type="text/javascript" src="js/index.js">changeTheText();</script>	
+	</div>
+	<div class="pics">
+	<img src="gfx/leonadro3.jpg" alt="Leonardo Da Vinci">
+	<img src="gfx/tesla4.jpg" alt="Nikola Tesla">
+	<img src="gfx/stieve1.jpg" alt="Stieve Jobs">
+	
+	
+	
+	</div>
+	
+	
+	<div class="set"></div>
+	<div class="fresh"></div>
+	<div class="news"></div>
+	
 </div>
 
 
