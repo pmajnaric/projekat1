3dbd80f8cee9ea57f95e454066d02af110cf727c
pmajnaric <paulina.majnaric1@gmail.com>
2016-07-19 23:54:36 +0200

    .

---
+++ template/header.tpl.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+
+<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+		<title><?= App::getTitle()?></title>
+		<link rel="icon"  href="gfx/planeta-1.ico"/>
+		<!--slika za browswer moze biti max 24px kockasta ili ikona(.ico)-->
+<?php
+		foreach(App::getCss() as $key=>$val)
+		{
+			echo"<link rel=\"stylesheet\" type=\"text/css\" href=\"css/{$val}.css?".time()."\">";
+		}
+
+
+?>
+<!--
+<style>
+body {
+    background-image: url("gfx/poz-3.jpg");
+}
+</style>
+-->
+</head>
+
+<body>
+
