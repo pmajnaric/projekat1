bb8114672a64561226c7a0c14f0f63bc2c1c1598
pmajnaric <paulina.majnaric1@gmail.com>
2016-08-30 23:31:54 +0200

    menu

--- admin/index.php
+++ admin/index.php
@@ -1,13 +1,11 @@
+
+
+
 <?php
-
 error_reporting(E_ALL);
-
 require_once "../include/app.php";
-include "../template/msg.tpl.php";
-
-
-
-
+//include "../template/msg.tpl.php"; //sta ce ovo ovde, na kraju ide template i zbog ovoga ti se pre docutype pojave poruke a to ne sme, ovo sam prebacio u template, i nemoj da vuces css i template sa front
+//Dooobroo :))
 
 $admin_users= new Admin_users();
 
@@ -17,15 +15,9 @@ if(!$admin_users->is_logged())
 	exit();
 }
 
-
-
-
 App::setTitle('Admin-login');
 App::setCss(['main']);
-App::template(['header']);
-
-
-
+App::template(['header', 'menu', 'msg']);
 
 
 ?>
---
+++ admin/template/menu.tpl.php
@@ -0,0 +1,67 @@
+
+<div id="nav">
+<ul>
+
+<li><a href="http://www.allbloggingtips.com/">Home</a></li>
+ <li><a href="#">Kategorije</a>
+            <ul>
+				<li><a href="#">Nauka</a>
+				<ul>
+					<li><a href="#">Elektrotehnika</a></li>
+					<li><a href="#">Medicina</a></li>
+					<li><a href="#">IT</a></li>
+					<li><a href="#">Filozofija</a></li>
+					<li><a href="#">Biologija</a></li>
+					<li><a href="#">Fizika</a></li>
+					<li><a href="#">Ostalo</a></li>
+				</ul>
+				</li>
+                	<li><a href="#">Istorija</a></li>
+	               <li><a href="#">Kultura</a>
+                	<ul>
+                		<li><a href="#">Knjizevnost</a></li>
+                		<li><a href="#">Pevanje</a></li>
+                		<li><a href="#">Gluma</a></li>
+              	 	</ul>
+                 </li>
+                    <li><a href="#">Politika</a>
+                    <li><a href="#">Sport</a>
+                    <ul>
+                        <li><a href="#">Tenis</a></li>
+                        <li><a href="#">Fudbal</a></li>
+                        <li><a href="#">Kosarka</a></li>
+                        <li><a href="#">Atletika</a></li>
+                        <li><a href="#">Umetnicko klizanje</a></li>
+                        <li><a href="#">Ostalo</a></li>
+                    </ul>
+                </li>                                 
+                <li><a href="#">Moda</a></li>
+                <li><a href="#">Biznis</a></li>
+                <li><a href="#">Ljudi koje poznajem</a></li>
+            </ul>
+        </li>
+ <li><a href="#">Veliki ljudi i vazni dogadaji</a></li>
+      
+ <li><a href="#">Preporuka</a>
+            <ul>
+				<li><a href="#">Film o velikim ljudima</a></li>
+				<li><a href="#">Film vaznim dogadajima</a></li>
+                <li><a href="#">Knjiga o velikim ljudima</a></li>
+                <li><a href="#">Knjiga vaznim dogadajima</a></li>
+                <li><a href="#">Pozorisna predstava</a></li>
+             </ul>
+  </li>
+                   
+ <li><a href="#">Psihologija</a></li>
+                    
+ <li><a href="#">Vesti</a></li>
+<li><a href="#">Galerija</a>
+ <ul>
+				<li><a href="#">Velikani</a></li>
+	 			<li><a href="#">Velikani i ja</a></li>
+</ul>
+<li><a href="#">Registracija korisnika</a></li>
+<li><a href="#">Moj profil</a></li>
+<li><a href="#">Logout</a></li>
+	</ul>
+</div>
