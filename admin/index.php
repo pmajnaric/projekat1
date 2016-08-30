07b158090a9a0bf4414dfcac3b6889ed2042a08a
pmajnaric <paulina.majnaric1@gmail.com>
2016-08-30 23:48:06 +0200

    .

--- admin/index.php
+++ admin/index.php
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-=======
 <?php
 
 error_reporting(E_ALL);
 
 require_once "../include/app.php";
 //include "../template/msg.tpl.php"; //sta ce ovo ovde, na kraju ide template i zbog ovoga ti se pre docutype pojave poruke a to ne sme, ovo sam prebacio u template, i nemoj da vuces css i template sa front
-
-
->>>>>>> 36a2ae9fee7ebb6becfe0a6e7f9fce81777a7776
-
 
 
 <?php
@@ -29,12 +23,6 @@ if(!$admin_users->is_logged())
 App::setTitle('Admin-login');
 App::setCss(['main']);
 App::template(['header', 'menu', 'msg']);
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 36a2ae9fee7ebb6becfe0a6e7f9fce81777a7776
 
 
 ?>
